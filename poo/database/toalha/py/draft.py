8da4066b1cc9188df35447c5c822be91d1655e51
joaohelioss <118625651+joao-helio-cpu@users.noreply.github.com>
2025-09-16 20:25:59 +0000

    ajeitandoToalha

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,1 +1,10 @@
-print ('olha ai')
+class T:
+    def __init__(self, color:str, size:str) :
+    
+        self.color = color
+        self.size = size
+        self.wetness: int = 0
+
+toalha = T("green", "g")
+print(toalha.color)
+print(toalha.size)
