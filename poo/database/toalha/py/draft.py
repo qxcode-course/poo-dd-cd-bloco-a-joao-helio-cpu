4878332cfb06c214e261d7aaf1f1207ee9cdc86e
joaohelioss <118625651+joao-helio-cpu@users.noreply.github.com>
2025-09-23 12:21:03 +0000

    Main Toalha quase Completo

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,10 +1,59 @@
 class T:
-    def __init__(self, color:str, size:str) :
-    
-        self.color = color
-        self.size = size
+    def __init__(self, color: str, size: str):
+        self.color: str = color
+        self.size: str = size
         self.wetness: int = 0
 
-toalha = T("green", "g")
-print(toalha.color)
-print(toalha.size)
+    def getMaxWetness(self):
+        if self.size == "P":
+            return 10
+        elif self.size == "M":
+            return 20
+        elif self.size == "G":
+            return 30
+        return 0 # se não for P, M ou G
+            
+    def dry(self, amount:int):
+        self.wetness += amount
+        if self.wetness > self.getMaxWetness():
+            print("A toalha está encharcada!")
+            self.wetness = self.getMaxWetness()
+            
+
+    def wringOut(self):
+        self.wetness = 0
+
+    def isDry(self) -> bool :
+            return self.wetness == 0
+
+    def __str__(self):
+        return f"Toalha {self.color} de tamanho {self.size} com umidade atual {self.wetness}"
+    
+
+def main():
+    toalha = T("", "")
+    while True:
+        line = input()
+        args = line.split(" ")
+
+        if args[0] == "end":
+            break
+
+        elif args[0] == "criar":
+            color = args[1]
+            size = args[2]
+            toalha = T(color, size)
+
+        elif args[0] == "ver":
+            print(toalha)
+
+        elif args[0] == "dry":
+            amount = int(args[1])
+            toalha.dry(amount)
+
+        
+
+        else:
+            print("Comando inválido")
+
+main()
